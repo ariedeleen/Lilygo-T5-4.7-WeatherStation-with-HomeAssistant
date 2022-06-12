007aedbffc031cf784dc883fe1847f8375b59f6d
Arie de Leen <github@orkey.nl>
2022-06-12 19:51:51 +0200

    Create lang_nl.h
    Dutch translation. There are different maybe preferred names for the moon fases ( https://nl.wikipedia.org/wiki/Schijngestalte ). Also the names of the weekday's are normally shorted by two alfabet letters imho. Or with a dot at the end. And written lower caps. ( https://onzetaal.nl/taalloket/afkortingen-dagen-en-maanden )

---
+++ src/lang_nl.h
@@ -0,0 +1,59 @@
+#define FONT(x) x##_tf
+
+//Temperature - Humidity - Forecast
+const String TXT_TEMPERATURE_C    = "Temperatuur (°C)";
+const String TXT_TEMPERATURE_F    = "Temperatuur (°F)";
+const String TXT_HUMIDITY_PERCENT = "Luchtvochtigheid (%)";
+const String TXT_HILO             = "Ho/La";
+const String TXT_FEELSLIKE        = "GW";
+
+// Pressure
+const String TXT_PRESSURE_HPA     = "Luchtdruk (hPa)";
+const String TXT_PRESSURE_IN      = "Luchtdruk (in)";
+
+//RainFall / SnowFall
+const String TXT_RAINFALL_MM = "Regen (mm)";
+const String TXT_RAINFALL_IN = "Regen (in)";
+const String TXT_SNOWFALL_MM = "Sneeuw (mm)";
+const String TXT_SNOWFALL_IN = "Sneeuw (in)";
+
+//Moon
+const String TXT_MOON_NEW             = "Nieuwe Maan";
+const String TXT_MOON_WAXING_CRESCENT = "Jonge Maansikkel";
+const String TXT_MOON_FIRST_QUARTER   = "Eerste Kwartier";
+const String TXT_MOON_WAXING_GIBBOUS  = "Wassende Maan";
+const String TXT_MOON_FULL            = "Volle Maan";
+const String TXT_MOON_WANING_GIBBOUS  = "Afnemende Maan";
+const String TXT_MOON_THIRD_QUARTER   = "Laatste Kwartier";
+const String TXT_MOON_WANING_CRESCENT = "Asgrauwe Maan";
+
+//Wind
+const String TXT_N   = "N";
+const String TXT_NNE = "NNO";
+const String TXT_NE  = "NO";
+const String TXT_ENE = "ONO";
+const String TXT_E   = "O";
+const String TXT_ESE = "OZO";
+const String TXT_SE  = "ZO";
+const String TXT_SSE = "ZZO";
+const String TXT_S   = "Z";
+const String TXT_SSW = "ZZW";
+const String TXT_SW  = "ZW";
+const String TXT_WSW = "WZW";
+const String TXT_W   = "W";
+const String TXT_WNW = "WNW";
+const String TXT_NW  = "NW";
+const String TXT_NNW = "NNW";
+
+// UV
+const String TXT_UV_LOW = "(L)";
+const String TXT_UV_MEDIUM = "(M)";
+const String TXT_UV_HIGH = "(H)";
+const String TXT_UV_VERYHIGH = "(H+)";
+const String TXT_UV_EXTREME = "(H!)";
+
+//Day of the week
+const char* weekday_D[] = { "Zon", "Maa", "Din", "Woe", "Don", "Vri", "Zat" };
+
+//Month
+const char* month_M[] = { "Jan", "Feb", "Mrt", "Apr", "Mei", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec" };
